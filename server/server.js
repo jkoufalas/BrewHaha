b19fd1aa80807f5e81c8d092b52e82b58f039acc
jkoufalas <john@koufalas.com>
2022-11-14 13:34:00 +1030

    route resolve

--- server/server.js
+++ server/server.js
@@ -18,12 +18,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-/* if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-} */
+}
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
