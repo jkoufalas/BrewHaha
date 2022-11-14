9aaccb1b51031b3ebc177f229ef0f312c0560dac
jkoufalas <john@koufalas.com>
2022-11-14 13:31:01 +1030

    route test

--- server/server.js
+++ server/server.js
@@ -18,9 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
-}
+} */
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
